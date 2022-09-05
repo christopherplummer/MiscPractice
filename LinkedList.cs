b840b080eeecc331245f7b0115c25dc3bc2c45df
Chris Plummer <chrisplummer0194@gmail.com>
2022-09-05 14:17:30 -0400

    Initial LinkedList implementation

--- LinkedList.cs
+++ LinkedList.cs
@@ -4,7 +4,52 @@ public class LinkedList<T>
 
     public int Count() 
     {
-        return 0;
+        if (Head is null) return 0;
+        if (Head.Next is null) return 1;
+        var tail = FindTail(Head.Next);
+        return tail.Position + 1;
+    }
+
+    public void Add(T item) 
+    {
+        if (Head is null) 
+        {
+            Head = new Node<T> {Position = 0, Data = item};
+            return;
+        }
+        else if (Head.Next is null) 
+        {
+            Head.Next = new Node<T> {Position = 1, Data = item};
+            return;
+        }
+        var tail = FindTail(Head.Next);
+        tail.Next = new Node<T> { Position = tail.Position + 1, Data = item };
+    }
+
+    public Node<T> FindNodeByPosition(int position)
+    {
+        if (position == 0) return Head;
+        if (position == 1) return Head?.Next;
+        if (position == 2) return Head?.Next?.Next;
+
+        var foundNode = false;
+        Node<T> targetNode = Head?.Next?.Next?.Next;
+        while (!foundNode)
+        {
+            if (targetNode is null || targetNode.Position == position) 
+            {
+                foundNode = true;
+                break;
+            }
+
+            targetNode = targetNode.Next;
+        }
+        return targetNode;
+    }
+
+    private Node<T> FindTail(Node<T> node)
+    {
+        return node.Next is null ? node : FindTail(node.Next);
     }
 }
 
@@ -12,5 +57,5 @@ public class Node<T>
 {
     public int Position { get; set; }
     public T Data { get; set; }
-    public T Next { get; set; }
+    public Node<T> Next { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var linkedList = new LinkedList<string>();
+linkedList.Add("Yes");
+linkedList.Add("No");
+linkedList.Add("Maybe");
+linkedList.Add("So");
+linkedList.Add("Okay");
+linkedList.Add("Thanks");
+
+var target = linkedList.FindNodeByPosition(2);
+Console.WriteLine($"Target Data: {target.Data}");
+Console.WriteLine($"Total Count: {linkedList.Count()}");
