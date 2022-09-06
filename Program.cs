9081474b8e904f6f230d1d13d623f411e4dd4eac
John <johntbrooks92@gmail.com>
2022-09-06 12:32:38 -0500

    removed excessive declaration

--- Program.cs
+++ Program.cs
@@ -7,8 +7,7 @@ linkedList.Add("Okay");
 linkedList.Add("Thanks");
 
 DateTime date = DateTime.Now;
-int hour = date.Hour;
-int hourRemainder = hour % 6;
+int hourRemainder = date.Hour % 6;
     
 var target = linkedList.FindNodeByPosition(hourRemainder);
 Console.WriteLine($"Target Data: {target.Data}");
