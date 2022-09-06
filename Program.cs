ce61c10c2ced86dbce001586d700c601b239c058
John <johntbrooks92@gmail.com>
2022-09-06 11:51:20 -0500

    refactored to DateTime to change index hourly

--- Program.cs
+++ Program.cs
@@ -6,6 +6,10 @@ linkedList.Add("So");
 linkedList.Add("Okay");
 linkedList.Add("Thanks");
 
-var target = linkedList.FindNodeByPosition(2);
+DateTime date = DateTime.Now;
+int hour = date.Hour;
+int hourRemainder = hour % 6;
+    
+var target = linkedList.FindNodeByPosition(hourRemainder);
 Console.WriteLine($"Target Data: {target.Data}");
 Console.WriteLine($"Total Count: {linkedList.Count()}");
